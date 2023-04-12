bcc4aac1dd2c88755ff34fc90cfefaea14ae3178
hendryar <e1900337@helplive.edu.my>
2023-04-12 23:34:30 +0800

    -Added login page using jetstream -Vite config -basic routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
